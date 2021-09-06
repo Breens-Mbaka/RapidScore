9fc3b650b59fea97770345a95b968bc019fd13c1
Breens-Thomas <mbakabreens@gmail.com>
2021-09-06 11:29:00 +0300

    Put image logo in the actionbar

--- app/src/main/java/com/example/swiftscore/MainActivity.kt
+++ app/src/main/java/com/example/swiftscore/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.swiftscore
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        actionBar?.setDisplayShowCustomEnabled(true)
+        val inflater: LayoutInflater =
+            this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.custom_logo_actionbar, null)
+        actionBar?.customView = view
     }
 }
