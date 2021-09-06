a69661883e3832b156cc10b2761a31bbe05ab470
Breens-Thomas <mbakabreens@gmail.com>
2021-09-06 09:37:19 +0300

    Added animation files for transistion between fragments

---
+++ app/src/main/java/com/example/swiftscore/ui/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.swiftscore.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.swiftscore.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
